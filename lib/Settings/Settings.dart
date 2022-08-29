55b07f645a1e073be76eccb8e7072eb3acda330a
abdo <abdofarag047@gmail.com>
2022-08-29 19:52:23 +0200

    add edit Screen

--- lib/Settings/Settings.dart
+++ lib/Settings/Settings.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 
-class SettingsTabs extends StatelessWidget {
+import 'Language Bottom Sheet.dart';
+import 'Theme Bottom Sheet.dart';
 
+class SettingsTabs extends StatefulWidget {
+
+  @override
+  State<SettingsTabs> createState() => _SettingsTabsState();
+}
+
+class _SettingsTabsState extends State<SettingsTabs> {
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+
+  void showLanguageButtomSheet(){
+    showModalBottomSheet(context: context, builder: (buildContext){
+      return Language_Bottom_Sheet();
+    });
+  }
+
+  void showThemeButtomSheet(){
+    showModalBottomSheet(context: context, builder: (buildContext){
+      return Theme_Bottom_Sheet();
+    });
   }
 }
