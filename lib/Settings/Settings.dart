736f044ab36eb01143a13120d95eb9b39ada342b
abdo <abdofarag047@gmail.com>
2022-08-30 05:19:40 +0200

    add edit Screen

--- lib/Settings/Settings.dart
+++ lib/Settings/Settings.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'Language Bottom Sheet.dart';
+import 'SettingProviders.dart';
 import 'Theme Bottom Sheet.dart';
 
 class SettingsTabs extends StatefulWidget {
@@ -12,7 +14,49 @@ class SettingsTabs extends StatefulWidget {
 class _SettingsTabsState extends State<SettingsTabs> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var settingProvider = Provider.of<setting_providers>(context);
+
+    return Container(
+      padding: EdgeInsets.all(8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text('language'),
+          SizedBox(height: 4,),
+          InkWell(
+            onTap: (){
+              showLanguageButtomSheet();
+            },
+            child: Container(
+
+              decoration: BoxDecoration(
+                border: Border.all(color: Theme.of(context).primaryColor, width: 1,),
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.transparent,
+              ),
+              padding: EdgeInsets.all(8),
+              child:
+              Text('english'),
+            ),
+          ),
+          SizedBox(height: 16,),
+          Text('Theme'),
+          SizedBox(height: 4,),
+          InkWell(
+            onTap: showThemeButtomSheet,
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Theme.of(context).primaryColor, width: 1,),
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.transparent,
+              ),
+              padding: EdgeInsets.all(8),
+              child:
+              Text('light'),            ),
+          )
+        ],
+      ),
+    );
   }
 
   void showLanguageButtomSheet(){
